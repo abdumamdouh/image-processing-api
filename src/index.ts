75d498bf19ce07a1217ef9ef731173aeb883ed3a
abdumamdouh <eabdogfx@gmail.com>
2021-12-25 06:47:10 +0200

    prettier config

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import express, { Application, Request, Response } from 'express'
+import morgan from 'morgan'
+import * as dotenv from 'dotenv'
+
+
+    dotenv.config()
+
+const PORT = process.env.PORT || 3000
+
+// create an instance server
+const app: Application = express()
+
+// HTTP request logger middleware
+app.use(morgan('dev'))
+
+// add routing for / path
+app.get('/', (req: Request, res: Response) => {
+  res.json({
+    message: 'Hello World !🌍'
+  })
+})
+
+// start express server
+app.listen(PORT, () => {
+  console.log(`Server is starting at prot:${PORT}`)
+})
+
+export default app
