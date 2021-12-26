33595affacda0b1e741acece404d1c5c410ddd77
abdumamdouh <eabdogfx@gmail.com>
2021-12-26 07:41:53 +0200

    index of src folder

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,36 @@
-import express, { Application, Request, Response } from 'express'
-import morgan from 'morgan'
-import * as dotenv from 'dotenv'
+import express from 'express';
+import { Application, Request, Response } from 'express';
+import * as dotenv from 'dotenv';
+import morgan from 'morgan';
+import loggerMiddleware from './middleware/loggerMiddleware';
 
+// the enviorment variable
+dotenv.config();
 
-    dotenv.config()
+//set the Port variable to the value from the .env file or 5000
+const Port = process.env.PORT || 5000;
 
-const PORT = process.env.PORT || 3000
+// the app instance of the server
+const app: Application = express();
 
-// create an instance server
-const app: Application = express()
+//optional to use the custom middleware instead of morgan
+//uncomment this to use the custom middleware
+// app.use(loggerMiddleware);
 
-// HTTP request logger middleware
-app.use(morgan('dev'))
+// logger middleware provided by morgan
+// you can comment this and use the custom middleware
+app.use(morgan('dev'));
 
 // add routing for / path
-app.get('/', (req: Request, res: Response) => {
-  res.json({
-    message: 'Hello World !🌍'
-  })
-})
+app.get('/', (reqest: Request, response: Response) => {
+    response.json({
+        message: 'Hello World !🌍',
+    });
+});
 
 // start express server
-app.listen(PORT, () => {
-  console.log(`Server is starting at prot:${PORT}`)
-})
+app.listen(Port, () => {
+    console.log(`the server is running at PORT:${Port}`);
+});
 
-export default app
+export default app;
